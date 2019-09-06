173a667c8ca6aef7c81579f095d75c5bc1f4cd1a
Harrish S <harrish.s@hotmail.ca>
2019-09-06 23:23:44 +0000

    completed

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let num = 0;
+    let current = this;
 
+    while (current.creator) {
+      current = current.creator;
+      num++;
+    }
+    return num;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.creator === null) {
+      return true;
+    } else if (vampire.creator == null) {
+      return false;
+    } else {
+      return this.creator.isMoreSeniorThan(vampire.creator);
+    }
   }
 
   /** Stretch **/
@@ -35,10 +49,19 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
 
-module.exports = Vampire;
+const original = new Vampire("original", 0);
+const ansel = new Vampire("Ansel", 1);
+const bart = new Vampire("Bart", 1);
+original.addOffspring(ansel);
+original.addOffspring(bart);
+const elgort = new Vampire("Elgort", 2);
+const sarah = new Vampire("Sarah", 2);
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+const andrew = new Vampire("Andrew", 3);
+elgort.addOffspring(andrew);
 
+module.exports = Vampire;
